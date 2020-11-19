c7924e07ba4861949a7ebd069b1981a0ff999d37
Nathan Cheung <nathancheung22@gmail.com>
2020-11-19 02:07:11 -0500

    added check if application is already setup

--- setup.py
+++ setup.py
@@ -3,23 +3,35 @@ from getpass import getpass
 
 # shameless plug
 print("\nurvianoob's Password Manager v1.0\n")
-print("Welcome to the initial setup\n")
 
-password = getpass(prompt="Enter your master password: ")
-confirmPassword = getpass(prompt="Confirm your master password: ")
-while password != confirmPassword or len(password) < 15:
-    if len(password) < 15:
-        print("\nPlease enter a password that's 15 characters or longer")
-    else:
-        print("\nError: passwords did not match\n")
+try:
+    # if you have these files, then you've already run the setup
+    with open("password.txt", "rb") as f:
+        f.read()
+    with open("definitelyNotWhereYourPasswordsAreStored.json", "rb") as f:
+        f.read()
+
+    print("Error: you've already setup the password-manager\n")
+
+except FileNotFoundError:
+    # if files not found, run the setup
+    print("Welcome to the initial setup\n")
 
     password = getpass(prompt="Enter your master password: ")
     confirmPassword = getpass(prompt="Confirm your master password: ")
+    while password != confirmPassword or len(password) < 15:
+        if len(password) < 15:
+            print("\nPlease enter a password that's 15 characters or longer")
+        else:
+            print("\nError: passwords did not match\n")
 
-x = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        password = getpass(prompt="Enter your master password: ")
+        confirmPassword = getpass(prompt="Confirm your master password: ")
 
-with open("password.txt", "wb") as f:
-    f.write(x)
+    x = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
-with open("definitelyNotWhereYourPasswordsAreStored.json", "wb") as f:
-    f.write(b'')
+    with open("password.txt", "wb") as f:
+        f.write(x)
+
+    with open("definitelyNotWhereYourPasswordsAreStored.json", "wb") as f:
+        f.write(b'')
